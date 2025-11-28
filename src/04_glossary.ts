cb61cdfea4d05947ec2b7e1d32a2db1d93d50f1f
Anahi Perez <anahiperezdsm@gmail.com>
2025-11-28 12:48:51 -0300

    agrego glosario de terminos

---
+++ src/04_glossary.ts
@@ -0,0 +1,20 @@
+
+export const GLOSSARY = [
+  {
+    term: "tipo union",
+    definition: "permite usar mas de un tipo",
+    example: "let id: string | number = 'a'"
+  },
+  {
+    term: "interface",
+    definition: "define forma de un objeto",
+    example: "interface p { name: string }"
+  },
+  {
+    term: "enum",
+    definition: "lista de valores constantes",
+    example: "enum color { red, blue }"
+  }
+];
+
+console.log("glosario:", GLOSSARY);
